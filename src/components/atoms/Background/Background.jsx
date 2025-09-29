5e3063462a174979e2d13cfc3b6ac15be9ebbb59
margar3tas <k.mikae.lieb@gmail.com>
2025-09-29 17:10:48 +0100

    fix bg

--- src/components/atoms/Background/Background.jsx
+++ src/components/atoms/Background/Background.jsx
@@ -1,6 +1,6 @@
 const Background = () => {
   return (
-    <div className='bg-[url(/assets/imgs/library-interior.jpg)] bg-no-repeat bg-cover fixed inset-0 opacity-50 bg-center blur-[2px]' />
+    <div className='bg-[url(/assets/imgs/library-interior.jpg)] bg-no-repeat bg-cover fixed inset-0 opacity-50 bg-center blur-[2px] h-dvh' />
   );
 };
 
